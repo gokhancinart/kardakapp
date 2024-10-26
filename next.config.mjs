ec881c099ff0bde896afd1e450c8317c6d099339
gokhancinart <gkn.cinar@gmail.com>
2024-10-26 03:54:21 +0300

    vercel i18n update

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'export', // Statik export için eklendi
   i18n: {
     locales: ['en', 'tr', 'de'],  // Desteklenen diller
     defaultLocale: 'tr',          // Varsayılan dil (path olmamasını istediğimiz dil)
