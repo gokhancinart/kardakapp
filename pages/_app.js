84debe18bdf085186650344da86bce76144d5f98
gokhancinart <gkn.cinar@gmail.com>
2024-10-27 18:02:19 +0300

    speed insight

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,15 @@
 import "../styles/globals.css";
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 import { appWithTranslation } from 'next-i18next';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <SpeedInsights />
+    </>
+  );
 }
 
 export default appWithTranslation(MyApp);
